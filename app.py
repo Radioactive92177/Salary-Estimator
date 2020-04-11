9e82a827a00044dd5efa3917783200be1357df74
Raj <56403033+Radioactive92177@users.noreply.github.com>
2020-04-12 03:56:43 +0530

    pretty printing 1 added functions to do the work

--- app.py
+++ app.py
@@ -5,6 +5,32 @@ from sklearn.linear_model import LinearRegression
 
 speak = wc.Dispatch("Sapi.SpVoice")
 
+
+def showData(X, y, x_name, y_name):
+    plt.scatter(X, y, c=X)
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
+    plt.show()
+
+
+def predictSalary(X, y, x_name):
+    model = LinearRegression()
+    model.fit(X, y)
+
+    coefficient = model.coef_
+    intercept = model.intercept_
+
+    speak.Speak(f"Please provide the the {x_name}")
+    exp = float(input(f"Enter {x_name} :"))
+
+    max_salary = (coefficient * exp)[0] + intercept
+    max_salary = int(max_salary)
+
+    speak.Speak(f"The maximum salary you should offer is around {max_salary}")
+
+    print(f"* Maximum Salary : {max_salary} *")
+
+
 print("******************************************************")
 print("           Welcome to the Salary Estimator            ")
 print("******************************************************")
@@ -39,23 +65,13 @@ y = data[y_name]
 
 X = x.values.reshape(len(x), 1)
 
-plt.scatter(X, y, c=X)
-plt.xlabel(x_name)
-plt.ylabel(y_name)
-plt.show()
+speak.Speak("Do you want to see the graph of your data?")
+choice_graph = input("Press Y to see Graph and N to ignore and proceed to prediction ")
 
-model = LinearRegression()
-model.fit(X, y)
+choice_graph = choice_graph.lower()
 
-coefficient = model.coef_
-intercept = model.intercept_
-
-speak.Speak(f"Please provide the the {x_name}")
-exp = float(input(f"Enter {x_name} :"))
-
-max_salary = (coefficient * exp)[0] + intercept
-max_salary = int(max_salary)
-
-speak.Speak(f"The maximum salary you should offer is around {max_salary}")
-
-print(f"* Maximum Salary : {max_salary} *")
+if choice_graph == "y":
+    showData(X, y, x_name, y_name)
+    predictSalary(X, y, x_name)
+else:
+    predictSalary(X, y, x_name)
