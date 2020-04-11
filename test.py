98fe24f2130b98398ad1d54f9ccd3dc3029487a2
Raj <56403033+Radioactive92177@users.noreply.github.com>
2020-04-11 11:35:15 +0530

    Data Visualized

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
 
 dataset = pd.read_csv('Dataset/SalaryData.csv')
 
@@ -13,4 +15,8 @@ x = dataset['YearsExperience']
 
 X = x.values.reshape(30,1)
 
-print(X)
+#Visualizing Data Here
+plt.scatter(X,y,c=X)
+plt.ylabel('Salary')
+plt.xlabel('Years of Experience')
+plt.show()
